5e5b218857890ca113e1b70d010f7d3cf6d7a79c
n-ce <abrocks1120@gmail.com>
2025-07-03 14:26:52 +0530

    use

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-import { createSignal, For } from 'solid-js';
+import { createSignal, Show } from 'solid-js';
 import './style.css';
 
 render(
@@ -8,60 +8,68 @@ render(
   document.body.firstElementChild as HTMLDivElement
 );
 
-function App() {
-  const [siblings, setSiblings] = createSignal<number[]>([]);
-  let siblingCounter = 0;
-
-  const sectionRefs: { [key: number]: HTMLDivElement | undefined } = {};
-
-  const openNewSection = () => {
-    const newId = siblingCounter++;
-    setSiblings(prev => [...prev, newId]);
-
-    sectionRefs[newId].scrollIntoView({ behavior: 'smooth', block: 'start' });
-  };
-
-  const closeSection = (idToClose: number) => {
-    setSiblings(prev => prev.filter(id => id !== idToClose));
-    delete sectionRefs[idToClose];
-  };
-
+function Sibling1(props) {
   return (
-    <>
-      <Head openNewSection={openNewSection}
-      />
-      <For each={siblings()}>
-        {(id) => (
-          <Sibling
-            id={id}
-            closeSection={closeSection}
-            ref={(el) => (sectionRefs[id] = el as HTMLDivElement)}
-          />
-        )}
-      </For>
-    </>
-  );
-}
-
-function Head(props: {
-  openNewSection: () => void
-}) {
-  return (
-    <section >
-      <button onClick={props.openNewSection}>Open a new section</button>
+    <section ref={props.ref}>
+      <button onClick={() => props.closeSection(props.id)}>Close Sibling 1</button>
     </section>
   );
 }
 
-function Sibling(props: {
-  id: number,
-  closeSection: (id: number) => void,
-  ref?: (el: HTMLDivElement) => void
-}
-) {
+function Sibling2(props) {
   return (
     <section ref={props.ref}>
-      <button onClick={() => props.closeSection(props.id)}>Close section #{props.id}</button>
+      <button onClick={() => props.closeSection(props.id)}>Close Sibling 2</button>
+    </section>
+  );
+}
+
+function App() {
+  const [activeSiblingIds, setActiveSiblingIds] = createSignal([]);
+  const sectionRefs = {};
+  let headRef: HTMLDivElement;
+
+  const openSibling = (idToOpen) => {
+    if (!activeSiblingIds().includes(idToOpen))
+      setActiveSiblingIds(prev => [...prev, idToOpen]);
+
+    sectionRefs[idToOpen].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+
+  const closeSection = async (idToClose) => {
+    headRef.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+    setTimeout(() => {
+      setActiveSiblingIds(prev => prev.filter(id => id !== idToClose).sort((a, b) => a - b));
+      delete sectionRefs[idToClose];
+    }, 500);
+  };
+
+  return (
+    <>
+      <Head openSibling={openSibling} ref={(el) => (headRef = el)} />
+
+      <Show when={activeSiblingIds().includes(1)}>
+        <Sibling1 id={1} closeSection={closeSection} ref={(el) => (sectionRefs[1] = el)} />
+      </Show>
+      <Show when={activeSiblingIds().includes(2)}>
+        <Sibling2 id={2} closeSection={closeSection} ref={(el) => (sectionRefs[2] = el)} />
+      </Show>
+    </>
+  );
+}
+
+function Head(props) {
+  return (
+    <section ref={props.ref}>
+      <button onClick={() => props.openSibling(1)}>Open Sibling 1</button>
+      <button onClick={() => props.openSibling(2)}>Open Sibling 2</button>
     </section>
   );
 }
